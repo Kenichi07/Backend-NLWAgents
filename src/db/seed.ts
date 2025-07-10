eb12426807bfe8b698241faf031f1da219dad637
Vinicius Huang <viniciushuangbr@gmail.com>
2025-07-09 21:51:18 -0300

    feat: add create question, create room and get room questions

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,20 +1,24 @@
 import { reset, seed } from 'drizzle-seed';
 import { db, sql } from './connection.ts';
 import { schema } from './schema/index.ts';
+import { questions } from './schema/questions.ts';
 
 await reset(db, schema);
 
-await seed(db, schema).refine( f => {
+await seed(db, schema).refine((f) => {
   return {
     rooms: {
       count: 20,
       columns: {
         name: f.companyName(),
         description: f.loremIpsum(),
-      }
-    }
-  }
-})
+      },
+    },
+    questions: {
+      count: 20,
+    },
+  };
+});
 
 await sql.end();
 
